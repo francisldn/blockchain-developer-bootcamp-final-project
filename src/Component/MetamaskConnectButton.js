e41762f4f192a64842ff34cab61d56a997980ab7
francisldn <fnhh04@gmail.com>
2021-11-28 08:04:57 +0000

    update Components

--- src/Component/MetamaskConnectButton.js
+++ src/Component/MetamaskConnectButton.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import detectEthereumProvider from '@metamask/detect-provider';
 import {ethers} from 'ethers'
 import '../App.css'
 
@@ -8,11 +7,19 @@ export const MetamaskConnectButton = () => {
     const[defaultAccount, setDefaultAccount]=useState(null);
     const[userBalance, setUserBalance]=useState(null);
     const[connButtonText, setConnButtonText] = useState('Connect Wallet');
+	const[networkMessage, setNetworkMessage] = useState("")
 
 
-    const connectWalletHandler =() => {
+    const connectWalletHandler = async () => {
         if (window.ethereum && window.ethereum.isMetaMask) {
 			console.log('MetaMask Here!');
+
+			let chainId= await window.ethereum.request({method: 'eth_chainId'})
+			if(chainId!== '0x4') {
+				setNetworkMessage("This dapp only works on the Rinkeby testnet for now. Please switch to Rinkeby to interact with the dapp.");
+			} else {
+				setNetworkMessage("");
+			}
 
 			window.ethereum.request({ method: 'eth_requestAccounts'})
 			.then(result => {
@@ -63,8 +70,9 @@ export const MetamaskConnectButton = () => {
                 <h5> Address: {defaultAccount}</h5>
             </div>
             <div className="balanceDisplay">
-                <h5>Balance: {userBalance}</h5>
+                <h5>Balance: {isNaN(Number.parseFloat(userBalance).toFixed(10)) ? "": Number.parseFloat(userBalance).toFixed(10) } ETH</h5>
             </div>
+			{networkMessage}
             {errorMessage}
         </div>
     )
